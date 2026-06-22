60641af5aa66ab9b8f95ed56c6f8fc5376dc23da
imanuelsteven <tugasstevengraciano@gmail.com>
2026-06-22 16:44:01 +0700

    Add Loan Applications

--- day-05-database-sql-jpa-hibernate-flyway/loanapplication/src/main/java/com/fif/loanapplication/controller/PaymentTransactionController.java
+++ day-05-database-sql-jpa-hibernate-flyway/loanapplication/src/main/java/com/fif/loanapplication/controller/PaymentTransactionController.java
@@ -2,6 +2,7 @@ package com.fif.loanapplication.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,6 @@ import com.fif.loanapplication.dto.payment.CreatePaymentTransactionRequest;
 import com.fif.loanapplication.dto.payment.PaymentTransactionResponse;
 import com.fif.loanapplication.service.PaymentTransactionService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
